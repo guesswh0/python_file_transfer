904ecdc2110e2aa80b1c54efd6f7a8b2e0d6c17b
dank0 <ohw0sseug@gmail.com>
2020-04-11 18:34:34 +0600

    update

--- client.py
+++ client.py
@@ -1,48 +1,57 @@
-import argparse
 import asyncio
 import os
-from pathlib import PurePosixPath
+import sys
 
+import click
 import tqdm
 
-from common import send_file, send_msg
 
+async def dispatcher(path, host, port, **kwargs):
+    files = sorted(os.listdir(path))
+    # check if path contains marker file
+    if not files[-1].endswith('.done'):
+        raise RuntimeError(f"'{path}' path does't contain marker file")
 
-async def client(path, host, port):
-    reader, writer = await asyncio.open_connection(host, port)
-    files = os.listdir(path)
-    files.sort()
+    reader, writer = await asyncio.open_connection(host, port, **kwargs)
+    marker_file = files.pop()
+    file_name = marker_file[:-5]
+    with open(os.path.join(path, marker_file), 'r') as marker:
+        file_size = int(marker.read())
 
-    # send file name
-    file_name = PurePosixPath(files[-1]).stem
-    await send_msg(writer, file_name)
-
-    # send file size
-    with open(os.path.join(path, files[-1]), 'r') as marker:
-        size = marker.read()
-        await  send_msg(writer, size)
+    # write package as [total_len|file_size|file_name]
+    writer.write((4 + len(file_name)).to_bytes(1, sys.byteorder))
+    writer.write(file_size.to_bytes(4, sys.byteorder))
+    writer.write(file_name.encode('utf-8'))
+    await writer.drain()
 
     # progress bar
-    progress = tqdm.tqdm(desc=f"Sending: {file_name}", total=len(files) - 1)
+    progress = tqdm.tqdm(desc=f"Sending: {file_name}", total=len(files))
 
     # send path files
-    for file in files[:-1]:
-        await send_file(writer, os.path.join(path, file))
+    for name in files:
+        file = os.path.join(path, name)
+        size = os.path.getsize(file)
+        writer.write(size.to_bytes(4, sys.byteorder))
+        with open(file, 'rb') as f:
+            writer.write(f.read())
+        await writer.drain()
         progress.update()
 
-    print("Close the socket")
+    progress.close()
     writer.close()
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('path', help="Files directory path")
-    parser.add_argument('--host', help="Server host", default='127.0.0.1')
-    parser.add_argument('--port', help="Server port", type=int, default=5001)
-    args = parser.parse_args()
-
+@click.command()
+@click.argument('path')
+@click.option('--host', help='server host', default='127.0.0.1')
+@click.option('--port', help='server port', type=int, default=5001)
+def client(path, host, port):
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(client(args.path, args.host, args.port))
+        loop.run_until_complete(dispatcher(path, host, port))
     except KeyboardInterrupt:
         loop.close()
+
+
+if __name__ == '__main__':
+    client()
--- server.py
+++ server.py
@@ -1,47 +1,77 @@
-import argparse
-import os
+import asyncio
 import gzip
+import logging
+import os
+import shutil
+import sys
+import tempfile
 
+import click
 import tqdm
 
-from common import run_server, read_file, read_msg
-
-# Working directory
-DIR = '.'
+logger = logging.getLogger(__name__)
 
 
-async def server(reader, writer):
-    addr = writer.get_extra_info('peername')
-    print(f"Client connected from {addr}")
+class FileServer:
+    def __init__(self, dir_, host, port, **kwargs):
+        self.files_dir = dir_
+        self.server_host = host
+        self.server_port = port
+        self.server = asyncio.start_server(
+            client_connected_cb=self.handle_client,
+            host=self.server_host,
+            port=self.server_port,
+            **kwargs
+        )
 
-    file_name = await read_msg(reader)
-    file_size = int(await read_msg(reader))
+    async def handle_client(self, reader, writer):
+        addr = writer.get_extra_info('peername')
+        logger.info("new connection from %s", addr)
 
-    progress = tqdm.tqdm(
-        range(file_size),
-        desc=f"Receiving: {file_name}",
-        unit='B',
-        unit_scale=True,
-        unit_divisor=1024
-    )
+        package_len = int.from_bytes(await reader.readexactly(1), sys.byteorder)
+        file_size = int.from_bytes(await reader.readexactly(4), sys.byteorder)
+        file_name = (await reader.readexactly(package_len - 4)).decode('utf-8')
 
-    with open(os.path.join(DIR, file_name), 'wb') as f:
+        progress = tqdm.tqdm(
+            range(file_size),
+            desc=f"Receiving: {file_name}",
+            unit='B',
+            unit_scale=True,
+            unit_divisor=1024
+        )
+
+        file = tempfile.NamedTemporaryFile(delete=False)
         while file_size:
-            data = gzip.decompress(await read_file(reader))
-            f.write(data)
+            size = int.from_bytes(await reader.readexactly(4), sys.byteorder)
+            data = gzip.decompress(await reader.readexactly(size))
+            file.write(data)
             progress.update(len(data))
             file_size -= len(data)
+        file.close()
+        shutil.move(file.name, os.path.join(self.files_dir, file_name))
 
-    print(f"Closing connection with {addr}")
-    writer.close()
+        logger.info("closing connection from %s", addr)
+        writer.close()
+
+
+@click.command()
+@click.option('--dir', help='working directory', default='out')
+@click.option('--host', help='server host', default='127.0.0.1')
+@click.option('--port', help='server port', type=int, default=5001)
+def server(dir, host, port):
+    file_server = FileServer(dir, host, port)
+    loop = asyncio.get_event_loop()
+    serv = loop.run_until_complete(file_server.server)
+    # Serve requests until Ctrl+C is pressed
+    print('Serving on {}'.format(serv.sockets[0].getsockname()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    serv.close()
+    loop.run_until_complete(serv.wait_closed())
+    loop.close()
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dir', help='Working directory', default='.')
-    parser.add_argument('--host', help='Server host', default='127.0.0.1')
-    parser.add_argument('--port', help='Server port', type=int, default=5001)
-    args = parser.parse_args()
-    DIR = args.dir
-
-    run_server(server, args.host, args.port)
+    server()
